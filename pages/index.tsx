e0942c8023479fa32224e67bbbb9584f16cc018e
musubi <musubi@theinternet.com>
2022-08-31 03:39:49 -0400

    add age until dumpage calc

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,31 @@ import type { NextPage } from "next";
 import { useState } from "react";
 
 import {
+  format,
   formatDistanceToNowStrict,
+  intervalToDuration,
   isAfter,
   isBefore,
   subDays,
 } from "date-fns";
 
+function calculateFullAge(birthDate: Date) {
+  const { years, months, days } = intervalToDuration({
+    start: birthDate,
+    end: new Date(),
+  });
+  return { years, months, days };
+}
 const Home: NextPage = () => {
   const [date, setDate] = useState<undefined | string>("");
   const [isEligible, setIsEligible] = useState<undefined | boolean>(undefined);
   const [ageMetadata, setAgeMetadata] = useState<{
     isTooYoung: boolean;
     isTooOld: boolean;
-    age: string;
+    ageObj: {
+      years: number;
+      months: number;
+    };
   }>({} as any);
   const [hover, setHover] = useState(false);
 
@@ -23,18 +35,24 @@ const Home: NextPage = () => {
       const minAge = subDays(new Date(), 365 * 21);
       const maxAge = subDays(new Date(), 365 * 25);
       const age = new Date(date);
+
       const isTooYoung = isAfter(age, minAge);
       const isTooOld = isBefore(age, maxAge);
       if (isTooOld || isTooYoung) {
         setIsEligible(false);
       } else {
+        const ageObj = calculateFullAge(age);
+        const ageUntilIneligible = {
+          years: 24 - ageObj.years!,
+          months: 12 - ageObj.months!,
+        };
         setIsEligible(true);
+        setAgeMetadata({
+          isTooYoung,
+          isTooOld,
+          ageObj: ageUntilIneligible,
+        });
       }
-      setAgeMetadata({
-        isTooYoung,
-        isTooOld,
-        age: formatDistanceToNowStrict(age),
-      });
     }
   };
   return (
@@ -79,7 +97,16 @@ const Home: NextPage = () => {
       )}
       <p style={{ fontSize: "25px" }}>Can you date Leonardo DiCaprio?</p>
       {isEligible && (
-        <p style={{ fontWeight: "bold" }}>Yes, you made the cut!</p>
+        <>
+          <p style={{ fontWeight: "bold" }}>Yes, you made the cut!</p>
+          <p>
+            You are{" "}
+            {ageMetadata.ageObj.years !== 0
+              ? `${ageMetadata.ageObj.years} years and `
+              : ""}
+            {ageMetadata.ageObj.months} months away from being dumped by Leo.
+          </p>
+        </>
       )}
       {!isEligible && (
         <>
